0bf911fa020f7bac4f81d024992999f5ffa793ba
rsgc-logush-o <ologush@rsgc.on.ca>
2015-11-15 22:36:35 -0500

    Did testing learnt how to write to the display
    I did some testing and learnt how to write to the display. I wrote a
    function that will write to the display. Now what I need to do is
    create a timer interrupt to update the display every 60 seconds. Also I
    will need to manage what will most likely be 8 bit colour using either
    bit angle modulation or pulse width modulation.

---
+++ FINAL_PROJECT_DISPLAY_DRIVER_CODE.ino
@@ -0,0 +1,77 @@
+int clockPin = 11;
+int latchPin = 14;
+int outputEnable = 15;
+int redData = 5;
+int blueData = 7;
+int greenData = 9;
+int redData1 = 6;
+int greenData1 = 10;
+int blueData1 = 8;
+int rowSelectA = 16;
+int rowSelectB = 17;
+int rowSelectC = 18;
+int rowSelectD = 19;
+
+void setup() {
+
+  pinMode(clockPin, OUTPUT);
+  pinMode(latchPin, OUTPUT);
+  pinMode(outputEnable, OUTPUT);
+  pinMode(redData, OUTPUT);
+  pinMode(blueData, OUTPUT);
+  pinMode(greenData, OUTPUT);
+  pinMode(redData1, OUTPUT);
+  pinMode(greenData1, OUTPUT);
+  pinMode(blueData1, OUTPUT);
+  pinMode(rowSelectA, OUTPUT);
+  pinMode(rowSelectB, OUTPUT);
+  pinMode(rowSelectC, OUTPUT);
+  pinMode(rowSelectD, OUTPUT);
+  
+
+}
+
+void loop() {
+
+
+ for(int i = 0; i < 16; i++)
+ {
+    updateRows(i, j,j,j,j,j,j);
+    
+}
+
+
+  
+
+}
+
+
+
+void updateRows(int row, uint32_t red, uint32_t green, uint32_t blue, uint32_t red1, uint32_t green1, uint32_t blue1 )
+{
+  digitalWrite(outputEnable, HIGH);
+  digitalWrite(latchPin, LOW);
+   digitalWrite(rowSelectA, row & B00000001);
+  digitalWrite(rowSelectB, row & B00000010);
+  digitalWrite(rowSelectC, row & B00000100);
+  digitalWrite(rowSelectD, row & B00001000);
+  for(int i = 0; i < 32; i++)
+  {
+    
+    
+    digitalWrite(redData, (red >> i) & 1);
+    digitalWrite(greenData, (green >> i) & 1);
+    digitalWrite(blueData, (blue >> i) & 1);
+    digitalWrite(redData1, (red1 >> i) & 1);
+    digitalWrite(greenData1, (green1 >> i) & 1);
+    digitalWrite(blueData1, (blue1 >> i) & 1);
+    digitalWrite(clockPin, HIGH);
+    digitalWrite(clockPin, LOW);
+   
+    
+  }
+ 
+  digitalWrite(latchPin, HIGH);
+  digitalWrite(outputEnable, LOW);
+}
+
